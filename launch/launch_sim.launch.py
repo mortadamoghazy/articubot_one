f9a4cd1f584bb1ee5028ee86cdb61a1decec29fd
mortadamoghazy <mortadaamrmoghazy@gmail.com>
2025-06-29 06:09:53 +0300

    ros2_control_1

--- launch/launch_sim.launch.py
+++ launch/launch_sim.launch.py
@@ -7,29 +7,20 @@ from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    package_name = 'articubot_one'
 
-    package_name = 'articubot_one'  # <-- Your package name
-
-    # Include the robot_state_publisher launch file
+    # Robot State Publisher
     rsp = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory(package_name),
-                'launch',
-                'rsp.launch.py'
-            )
+            os.path.join(get_package_share_directory(package_name), 'launch', 'rsp.launch.py')
         ]),
         launch_arguments={'use_sim_time': 'true'}.items()
     )
 
-    # Include the Gazebo launch file with canes.world
+    # Gazebo with cones.world
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([
-            os.path.join(
-                get_package_share_directory('gazebo_ros'),
-                'launch',
-                'gazebo.launch.py'
-            )
+            os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
         ]),
         launch_arguments={
             'world': os.path.join(
@@ -40,17 +31,34 @@ def generate_launch_description():
         }.items()
     )
 
-    # Spawn the robot in Gazebo
+    # Spawn robot in Gazebo
     spawn_entity = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description', '-entity', 'my_robot'],
+        arguments=['-topic', 'robot_description', '-entity', 'my_bot'],
         output='screen'
     )
 
-    # Launch everything
+    # Spawner for joint_state_broadcaster
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_broad"],
+        output="screen"
+    )
+
+    # Spawner for diff_drive_controller
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["diff_cont"],
+        output="screen"
+    )
+
     return LaunchDescription([
         rsp,
         gazebo,
         spawn_entity,
+        joint_broad_spawner,
+        diff_drive_spawner
     ])
